0c1c12380a7af6fd02c9fdf32de71743ef17ae15
Tim Martin <timmartin13@gmail.com>
2021-04-07 13:05:59 -0500

    Added resume in pdf format

--- src/components/Contact/ContactGlass.js
+++ src/components/Contact/ContactGlass.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./ContactGlassStyle.css";
 import profilePicture from '../../../src/images/ProfilePicture.jpg';
+import resumePDF from '../../../src/images/TimMartinResume.pdf';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import IconButton from '@material-ui/core/IconButton';
+import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
 import Tooltip from '@material-ui/core/Tooltip';
 
 function ContactGlass() {
@@ -21,17 +23,24 @@ function ContactGlass() {
               <GitHubIcon style={{ color: "purple" }} />
             </IconButton>
           </Tooltip>
-          <IconButton aria-label="linkedIn" href="https://www.linkedin.com/in/tim-martin13/" target="_blank">
-            <LinkedInIcon style={{ color: "blue" }} />
-          </IconButton>
+          <Tooltip title="tim-martin13" arrow>
+            <IconButton aria-label="linkedIn" href="https://www.linkedin.com/in/tim-martin13/" target="_blank">
+              <LinkedInIcon style={{ color: "blue" }} />
+            </IconButton>
+          </Tooltip>
           <Tooltip title="612-799-1450" arrow>
             <IconButton aria-label="phone number">
-              <PhoneAndroidIcon style={{ color: "green" }} />
+              <PhoneAndroidIcon style={{ color: "orange" }} />
             </IconButton>
           </Tooltip>
           <Tooltip title="timmartin13@gmail.com" arrow>
             <IconButton aria-label="email">
-                <MailOutlineIcon style={{ color: "red" }} />
+                <MailOutlineIcon style={{ color: "green" }} />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Resume" arrow>
+            <IconButton aria-label="resume" href={ resumePDF } target="_blank">
+                <PictureAsPdfIcon style={{ color: "red" }} />
             </IconButton>
           </Tooltip>
         </div>
